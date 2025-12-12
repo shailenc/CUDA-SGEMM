8a65205ed47bc79ca8957a95b2600e6ccd3dacb2
shailenc <shailen.chander@me.com>
2025-12-12 01:42:21 -0500

    Add project files
    Includes build script, and basic unoptimized matrix mult implementation

---
+++ src/matrix.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <type_traits>
+#include <cuda_runtime.h>
+
+/*
+	A matrix designed to have (most) operations run on the GPU via CUDA kernels.
+
+	This matrix is row major, and (for now) only supports floats.
+*/
+class SCMatrix
+{
+public:
+
+	const size_t height;
+	const size_t width;
+	const size_t num_elements;
+
+	SCMatrix(size_t input_height, size_t input_width);
+	~SCMatrix();
+
+	SCMatrix operator*(const SCMatrix& rhs) const;
+
+private:
+
+	float* data_ = nullptr;
+};
